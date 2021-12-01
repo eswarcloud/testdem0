4ceedfe1238667a68cc65ca573da327ce959ad76
Mammu <mamathamulapaku99@gmail.com>
2021-12-01 10:02:19 +0530

    Update

---
+++ src/main/java/com/tcs/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.tcs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		
+	}
+	@RequestMapping("/list_contact")
+	public String Welcome()
+	{
+		return ("COntact");
+		
+	}
+}
