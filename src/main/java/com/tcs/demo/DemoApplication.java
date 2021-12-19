7e4fe445396ff87ec9f5b1574c6c55817e9d7bfc
Mamatha Reddy <mamathamulapaku99@gmail.com>
2021-12-19 03:21:47 +0000

    Update src/main/java/com/tcs/demo/DemoApplication.java

--- src/main/java/com/tcs/demo/DemoApplication.java
+++ src/main/java/com/tcs/demo/DemoApplication.java
@@ -2,19 +2,12 @@ package com.tcs.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		
-	}
-	@RequestMapping("/list_contact")
-	public String Welcome()
-	{
-		return ("Contact");
 		
 	}
 }
